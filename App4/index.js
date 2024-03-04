2fd87f7d741b18271bd84af1ae5fca7eefd54c20
Rich <rickygb082002@gmail.com>
2024-03-04 01:30:14 -0600

    curso express

--- App4/index.js
+++ App4/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express()
+const morgan = require('morgan');
+
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-    res.sendFile('./public/index.html', {
+    res.sendFile('./public/index.html', { //pagina inicial
         root: __dirname
     })
+})
+
+
+
+app.get('/dashboard', (req, res) => {
+    res.send('Panel de control')
+})
+
+app.get('/pacientes', (req, res) => {
+    res.send('Pacientes')
 })
 
 app.listen(3000)
 
 
-console.log(`Server on port 3000 ${3000}`);
+console.log(`Server on port: ${3000}`);
