3b977c7c9f96d58572d5ca42d9d6eba79e234f5c
Rich <rickygb082002@gmail.com>
2024-03-11 14:13:57 -0600

    angular course

--- App6/src/controllers/pacientesController.js
+++ App6/src/controllers/pacientesController.js
@@ -5,21 +5,21 @@ const controller = {};
 // reportes
 controller.list = (req, res) => {
     req.getConnection((err, conn) => {
-        conn.query('SELECT * FROM pacientes;', (err, pacientes) => {
-            if(err){
-                next(err)
+        conn.query('SELECT * FROM pacientes;', (err, pacientesList) => {
+            if (err) {
+                next(err);
+                return;
             }
 
-            else{//vista
-                res.render('pacientes', {
-                    data: pacientes //variable data
-                }) 
-            }
-
-
-        })
-    })
+            // Renderizar la vista 'pacientes' con la lista de pacientes
+            res.render('pacientes', {
+                data: pacientesList // variable data
+            });
+        });
+    });
 };
+
+
 
 // dar de alta 
 controller.save = (req, res) => {
@@ -48,6 +48,91 @@ controller.save = (req, res) => {
 
         });
     });
+};
+
+
+
+// dar cambios
+controller.updateGet = (req, res) => { //btn obtener datos
+    const idPaciente = req.params.idPaciente;
+
+    req.getConnection((err, conn) => {
+        if(err){
+            console.error(err);
+            res.status(500).send('Error de conexión a la base de datos');
+            return;
+        }
+
+        else{
+            conn.query('SELECT * FROM pacientes WHERE id = ?', [idPaciente], (err, paciente) => {
+                res.render('pacientes_edit', {
+                    data: paciente[0]
+                })
+            })
+        }
+    })
+};
+
+controller.updatePost = (req, res) => { //btn actulizar
+    const idPaciente = req.params.idPaciente;
+    const dataUpate = req.body;
+
+    req.getConnection((err, conn) => {
+        if(err){
+            console.error(err);
+            res.status(500).send('Error de conexión a la base de datos');
+            return;
+        }
+        else{
+            conn.query('UPDATE pacientes SET ? WHERE ID = ?', [dataUpate, idPaciente], (err, updatePaciente) => {
+                if(err){
+                    console.error(err);
+                    res.status(500).send('Error al eliminar el paciente');
+                    return;
+                }
+
+                else{
+                    //agrega el paciente nuevo
+                    console.log("Paciente actualizado correctamente en la base de datos");
+                    res.redirect('/'); //redirige a la pagina incial
+                }
+            })
+        }
+    })
+    
+}
+
+
+
+
+
+
+// dar de baja
+controller.delete = (req, res) => {
+    const idPaciente = req.params.idPaciente;
+
+    req.getConnection((err, conn) => {
+        if(err){
+            console.error(err);
+            res.status(500).send('Error de conexión a la base de datos');
+            return;
+        }
+        else{
+            conn.query('DELETE FROM pacientes WHERE ID = ?', [idPaciente], (err, delPaciente) => {
+                if(err){
+                    console.error(err);
+                    res.status(500).send('Error al eliminar el paciente');
+                    return;
+                }
+
+                else{
+                    //agrega el paciente nuevo
+                    console.log("Paciente eliminado correctamente en la base de datos");
+                    res.redirect('/'); //redirige a la pagina incial
+                }
+            })
+        }
+    })
 };
 
 
--- App6/src/routes/pacientes.js
+++ App6/src/routes/pacientes.js
@@ -7,7 +7,9 @@ const router = express.Router()
 
 
 router.get('/', pacientesController.list)
-
-router.post('/add', pacientesController.save)
+router.post('/add', pacientesController.save) //Guardar registro datos del paciente
+router.get('/delete/:idPaciente', pacientesController.delete)  //Borrar datos del paciente
+router.get('/update/:idPaciente', pacientesController.updateGet)  //Obtener datos del paciente
+router.post('/update/:idPaciente', pacientesController.updatePost)  //Actualizar datos del paciente
 
 module.exports = router
