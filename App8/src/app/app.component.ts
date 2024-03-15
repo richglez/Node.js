bc84051d44daf7f2140a1dddfc5d2d0450e6717a
Rich <rickygb082002@gmail.com>
2024-03-15 11:32:05 -0600

    el bueno

--- App8/src/app/app.component.ts
+++ App8/src/app/app.component.ts
@@ -3,17 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Cambiado de styleUrl a styleUrls
 })
 
 export class AppComponent {
   users: string[] = ['rich', 'john', 'riichh']
 
-
   sayHello(){
     alert('Hello!')
   }
 }
-
-
-
