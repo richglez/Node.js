06e30a90465fe9d3746e2addaf8300fb02053920
Rich <rickygb082002@gmail.com>
2024-03-15 16:27:46 -0600

    app8

--- App8/src/app/app.component.ts
+++ App8/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from '../data.service';
+import { Post } from './Post';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,46 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   users: string[] = ['rich', 'john', 'riichh']
+  name: string = 'Ricardo'
+  age: number = 22
+  posts: Post[] = [];
 
-  sayHello(){
-    alert('Hello!')
+
+
+
+  constructor(private dataService: DataService){
+    this.dataService.getData().subscribe(data => {
+      console.log(data);
+      this.posts = data;
+      
+    })
   }
+
+  deleteUser(user: string){
+    for (let i = 0; i < this.users.length; i++) {
+      const usuerFind = this.users[i];
+      if (user === usuerFind) {
+        // usuario encontrado
+        this.users.splice(i, 1);
+      }
+    }
+
+  }
+
+  addUser(txtUser: HTMLInputElement) {
+    if (txtUser.value === '') {
+      alert('Cadena vacia')
+      txtUser.focus()
+    }
+    else{
+      this.users.push(txtUser.value);
+      txtUser.value = ''; // Limpiar el campo de entrada después de agregar el usuario
+      txtUser.focus()
+    }
+    
+    return false; // Para evitar que el formulario se envíe
+  }
+
+
+
 }
--- App8/src/app/app.module.ts
+++ App8/src/app/app.module.ts
@@ -1,21 +1,36 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Route } from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { DataService } from "../data.service";
+import { AboutComponent } from './about/about.component';
+
+const routes: Route[] = [
+  {path: '', component: AppComponent},
+  {path: 'about', component: AboutComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    DataService
   ],
   bootstrap: [AppComponent]
 })
