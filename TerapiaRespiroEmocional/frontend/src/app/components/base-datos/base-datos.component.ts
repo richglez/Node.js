f09b3310313d6a26ab973f33cdb72da987c734ea
Rich <rickygb082002@gmail.com>
2024-08-02 16:56:44 -0600

    base de datos v1

--- TerapiaRespiroEmocional/frontend/src/app/components/base-datos/base-datos.component.ts
+++ TerapiaRespiroEmocional/frontend/src/app/components/base-datos/base-datos.component.ts
@@ -12,6 +12,7 @@ import Swal from 'sweetalert2';
   selector: 'app-base-datos',
   templateUrl: './base-datos.component.html',
   styleUrls: ['./base-datos.component.scss'],
+  
 })
 export class BaseDatosComponent implements OnInit {
   searchText: string = '';
@@ -46,205 +47,6 @@ export class BaseDatosComponent implements OnInit {
       });
   }
 
-  /* Default name for excel file when download */
-  fileName1 = 'BaseDatosPacientes.xlsx';
-  fileName2 = 'BaseDatosCuidadores.xlsx';
-  fileName3 = 'BaseDatosSuplencias.xlsx';
-  exportExcel() {
-    let data;
-    let fileName;
-    switch (this.selectedCategory) {
-      case 'pacientes':
-        data = document.getElementById('BaseDatosPacientes');
-        fileName = this.fileName1;
-        break;
-      case 'cuidadores':
-        data = document.getElementById('BaseDatosCuidadores');
-        fileName = this.fileName2;
-        break;
-      case 'suplencias':
-        data = document.getElementById('BaseDatosSuplencias');
-        fileName = this.fileName3;
-        break;
-      default:
-        return;
-    }
-
-    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(data);
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-    XLSX.writeFile(wb, fileName);
-  }
-
-  onSearchChange(): void {
-    //para recargar los datos cuando cambie la selección DE BUSQUEDA.
-    this.searchTextChanged.next(this.searchText);
-  }
-
-  onSexoChange(): void {
-    //para recargar los datos cuando cambie la selección de sexo.
-    this.loadCategoryData();
-  }
-
-  search(): void {
-    this.loadCategoryData();
-  }
-
-  loadCategoryData(): void {
-    if (this.selectedCategory === 'pacientes') {
-      this.pacientesService.getNombreCuidadorDelPaciente().subscribe(
-        (data) => {
-          console.log('Datos de pacientes:', data);
-          this.pacientes = data;
-          this.filteredPacientes = this.filterBySexo(data); // Aplica el filtrado por sexo
-        },
-        (error) => {
-          console.error('Error al cargar pacientes:', error);
-        }
-      );
-    } else if (this.selectedCategory === 'cuidadores') {
-      this.cuidadoresService.getCuidadores().subscribe(
-        (data) => {
-          console.log('Datos de cuidadores:', data);
-          this.cuidadores = data;
-          this.filteredCuidadores = this.filterBySexo(data); // Aplica el filtrado por sexo
-        },
-        (error) => {
-          console.error('Error al cargar cuidadores:', error);
-        }
-      );
-    } else if (this.selectedCategory === 'suplencias') {
-      this.suplenciasService.getNombreCuidadoryPaciente().subscribe(
-        (data) => {
-          console.log('Datos de suplencias:', data);
-          this.suplencias = data;
-          this.filteredSuplencias = data;
-        },
-        (error) => {
-          console.error('Error al cargar suplencias:', error);
-        }
-      );
-    }
-  }
-
-  removeAccents(str: string): string {
-    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-  }
-
-  filterBySexo(data: any[]): any[] {
-    if (!this.selectedSexo) {
-      // Si no se selecciona ningún filtro de sexo, retorna todos los datos.
-      return data;
-    }
-    return data.filter((item) => {
-      if (this.selectedCategory === 'pacientes') {
-        return item.sexo_paciente === this.selectedSexo;
-      } else if (this.selectedCategory === 'cuidadores') {
-        return item.sexoCuidador === this.selectedSexo;
-      }
-      return true;
-    });
-  }
-
-  filterData(searchText: string) {
-    const lowerSearchText = searchText.toLowerCase();
-    let filteredData: any[] = [];
-
-    if (this.selectedCategory === 'pacientes') {
-      const normalizedSearchText = this.removeAccents(lowerSearchText);
-
-      filteredData = this.pacientes.filter(
-        (paciente) =>
-          this.removeAccents(
-            (paciente.nombre_paciente || '').toLowerCase()
-          ).includes(normalizedSearchText) ||
-          this.removeAccents(
-            (paciente.apellido_paterno || '').toLowerCase()
-          ).includes(normalizedSearchText) ||
-          this.removeAccents(
-            (paciente.apellido_materno || '').toLowerCase()
-          ).includes(normalizedSearchText) ||
-          this.removeAccents(
-            (paciente.alcaldia_municipio || '').toLowerCase()
-          ).includes(normalizedSearchText) ||
-          this.removeAccents(
-            (paciente.entidadFederativa || '').toLowerCase()
-          ).includes(normalizedSearchText) ||
-          this.removeAccents(
-            (paciente.sexo_paciente || '').toLowerCase()
-          ).includes(normalizedSearchText) ||
-          this.removeAccents(
-            (paciente.tipoPrograma || '').toLowerCase()
-          ).includes(normalizedSearchText) ||
-          this.removeAccents(
-            (paciente.expediente_paciente || '').toLowerCase()
-          ).includes(normalizedSearchText)
-      );
-
-      // Aplica el filtro por sexo
-      filteredData = this.filterBySexo(filteredData);
-    } else if (this.selectedCategory === 'cuidadores') {
-      filteredData = this.cuidadores.filter(
-        (cuidador) =>
-          this.removeAccents(
-            (cuidador.nombreCuidador || '').toLowerCase()
-          ).includes(lowerSearchText) ||
-          this.removeAccents(
-            (cuidador.apPatCuidador || '').toLowerCase()
-          ).includes(lowerSearchText) ||
-          this.removeAccents(
-            (cuidador.apMatCuidador || '').toLowerCase()
-          ).includes(lowerSearchText) ||
-          this.removeAccents(
-            (cuidador.telefonoCuidador || '').toLowerCase()
-          ).includes(lowerSearchText)
-      );
-
-      // Aplica el filtro por sexo
-      filteredData = this.filterBySexo(filteredData);
-    } else if (this.selectedCategory === 'suplencias') {
-      filteredData = this.suplencias.filter(
-        (suplencia) =>
-          this.removeAccents(
-            (suplencia.dia_suplencia || '').toLowerCase()
-          ).includes(lowerSearchText) ||
-          this.removeAccents(
-            (suplencia.hora_inicial || '').toLowerCase()
-          ).includes(lowerSearchText) ||
-          this.removeAccents(
-            (suplencia.hora_final || '').toLowerCase()
-          ).includes(lowerSearchText) ||
-          this.removeAccents(
-            (suplencia.particular || '').toLowerCase()
-          ).includes(lowerSearchText) ||
-          this.removeAccents(
-            (suplencia.concurrencia_anual || '').toLowerCase()
-          ).includes(lowerSearchText) ||
-          this.removeAccents(
-            (suplencia.nombreCuidador || '').toLowerCase()
-          ).includes(lowerSearchText) ||
-          this.removeAccents(
-            (suplencia.apPatCuidador || '').toLowerCase()
-          ).includes(lowerSearchText) ||
-          this.removeAccents(
-            (suplencia.apMatCuidador || '').toLowerCase()
-          ).includes(lowerSearchText)
-      );
-    }
-
-    return of(filteredData);
-  }
-
-  applyFilteredData(filteredData: any): void {
-    if (this.selectedCategory === 'pacientes') {
-      this.filteredPacientes = filteredData;
-    } else if (this.selectedCategory === 'cuidadores') {
-      this.filteredCuidadores = filteredData;
-    } else if (this.selectedCategory === 'suplencias') {
-      this.filteredSuplencias = filteredData;
-    }
-  }
-
 
 
   //Pacientes
@@ -258,14 +60,12 @@ export class BaseDatosComponent implements OnInit {
   saveChangesPaciente(paciente: any, index: number): void {
     this.pacientesService.updatePaciente(paciente).subscribe(
       (response) => {
-        this.toggleEditPaciente(index, 'expediente');
-        this.toggleEditPaciente(index, 'apellido_paterno');
-        this.toggleEditPaciente(index, 'apellido_materno');
-        this.toggleEditPaciente(index, 'nombre');
+        this.editField[index] = {};
         this.snackBar.open('Datos actualizados correctamente', 'Cerrar', {
           duration: 3000,
           horizontalPosition: 'center',
-          verticalPosition: 'top',
+          verticalPosition: 'bottom',
+          panelClass: ['main-snackbar'],
         });
       },
       (error) => {
@@ -314,6 +114,8 @@ export class BaseDatosComponent implements OnInit {
       }
     });
   }
+
+
 
   //Cuidadores
   deleteCuidador(cuidadorId: number): void {
@@ -386,4 +188,206 @@ export class BaseDatosComponent implements OnInit {
       }
     });
   }
+
+  // Excel
+    /* Default name for excel file when download */
+    fileName1 = 'BaseDatosPacientes.xlsx';
+    fileName2 = 'BaseDatosCuidadores.xlsx';
+    fileName3 = 'BaseDatosSuplencias.xlsx';
+    exportExcel() {
+      let data;
+      let fileName;
+      switch (this.selectedCategory) {
+        case 'pacientes':
+          data = document.getElementById('BaseDatosPacientes');
+          fileName = this.fileName1;
+          break;
+        case 'cuidadores':
+          data = document.getElementById('BaseDatosCuidadores');
+          fileName = this.fileName2;
+          break;
+        case 'suplencias':
+          data = document.getElementById('BaseDatosSuplencias');
+          fileName = this.fileName3;
+          break;
+        default:
+          return;
+      }
+  
+      const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(data);
+      const wb: XLSX.WorkBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+      XLSX.writeFile(wb, fileName);
+    }
+
+    // Funcionalidades extras
+  
+    onSearchChange(): void {
+      //para recargar los datos cuando cambie la selección DE BUSQUEDA.
+      this.searchTextChanged.next(this.searchText);
+    }
+  
+    onSexoChange(): void {
+      //para recargar los datos cuando cambie la selección de sexo.
+      this.loadCategoryData();
+    }
+  
+    search(): void {
+      this.loadCategoryData();
+    }
+  
+    loadCategoryData(): void {
+      if (this.selectedCategory === 'pacientes') {
+        this.pacientesService.getNombreCuidadorDelPaciente().subscribe(
+          (data) => {
+            console.log('Datos de pacientes:', data);
+            this.pacientes = data;
+            this.filteredPacientes = this.filterBySexo(data); // Aplica el filtrado por sexo
+          },
+          (error) => {
+            console.error('Error al cargar pacientes:', error);
+          }
+        );
+      } else if (this.selectedCategory === 'cuidadores') {
+        this.cuidadoresService.getCuidadores().subscribe(
+          (data) => {
+            console.log('Datos de cuidadores:', data);
+            this.cuidadores = data;
+            this.filteredCuidadores = this.filterBySexo(data); // Aplica el filtrado por sexo
+          },
+          (error) => {
+            console.error('Error al cargar cuidadores:', error);
+          }
+        );
+      } else if (this.selectedCategory === 'suplencias') {
+        this.suplenciasService.getNombreCuidadoryPaciente().subscribe(
+          (data) => {
+            console.log('Datos de suplencias:', data);
+            this.suplencias = data;
+            this.filteredSuplencias = data;
+          },
+          (error) => {
+            console.error('Error al cargar suplencias:', error);
+          }
+        );
+      }
+    }
+  
+    removeAccents(str: string): string {
+      return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+    }
+  
+    filterBySexo(data: any[]): any[] {
+      if (!this.selectedSexo) {
+        // Si no se selecciona ningún filtro de sexo, retorna todos los datos.
+        return data;
+      }
+      return data.filter((item) => {
+        if (this.selectedCategory === 'pacientes') {
+          return item.sexo_paciente === this.selectedSexo;
+        } else if (this.selectedCategory === 'cuidadores') {
+          return item.sexoCuidador === this.selectedSexo;
+        }
+        return true;
+      });
+    }
+  
+    filterData(searchText: string) {
+      const lowerSearchText = searchText.toLowerCase();
+      let filteredData: any[] = [];
+  
+      if (this.selectedCategory === 'pacientes') {
+        const normalizedSearchText = this.removeAccents(lowerSearchText);
+  
+        filteredData = this.pacientes.filter(
+          (paciente) =>
+            this.removeAccents(
+              (paciente.nombre_paciente || '').toLowerCase()
+            ).includes(normalizedSearchText) ||
+            this.removeAccents(
+              (paciente.apellido_paterno || '').toLowerCase()
+            ).includes(normalizedSearchText) ||
+            this.removeAccents(
+              (paciente.apellido_materno || '').toLowerCase()
+            ).includes(normalizedSearchText) ||
+            this.removeAccents(
+              (paciente.alcaldia_municipio || '').toLowerCase()
+            ).includes(normalizedSearchText) ||
+            this.removeAccents(
+              (paciente.entidadFederativa || '').toLowerCase()
+            ).includes(normalizedSearchText) ||
+            this.removeAccents(
+              (paciente.sexo_paciente || '').toLowerCase()
+            ).includes(normalizedSearchText) ||
+            this.removeAccents(
+              (paciente.tipoPrograma || '').toLowerCase()
+            ).includes(normalizedSearchText) ||
+            this.removeAccents(
+              (paciente.expediente_paciente || '').toLowerCase()
+            ).includes(normalizedSearchText)
+        );
+  
+        // Aplica el filtro por sexo
+        filteredData = this.filterBySexo(filteredData);
+      } else if (this.selectedCategory === 'cuidadores') {
+        filteredData = this.cuidadores.filter(
+          (cuidador) =>
+            this.removeAccents(
+              (cuidador.nombreCuidador || '').toLowerCase()
+            ).includes(lowerSearchText) ||
+            this.removeAccents(
+              (cuidador.apPatCuidador || '').toLowerCase()
+            ).includes(lowerSearchText) ||
+            this.removeAccents(
+              (cuidador.apMatCuidador || '').toLowerCase()
+            ).includes(lowerSearchText) ||
+            this.removeAccents(
+              (cuidador.telefonoCuidador || '').toLowerCase()
+            ).includes(lowerSearchText)
+        );
+  
+        // Aplica el filtro por sexo
+        filteredData = this.filterBySexo(filteredData);
+      } else if (this.selectedCategory === 'suplencias') {
+        filteredData = this.suplencias.filter(
+          (suplencia) =>
+            this.removeAccents(
+              (suplencia.dia_suplencia || '').toLowerCase()
+            ).includes(lowerSearchText) ||
+            this.removeAccents(
+              (suplencia.hora_inicial || '').toLowerCase()
+            ).includes(lowerSearchText) ||
+            this.removeAccents(
+              (suplencia.hora_final || '').toLowerCase()
+            ).includes(lowerSearchText) ||
+            this.removeAccents(
+              (suplencia.particular || '').toLowerCase()
+            ).includes(lowerSearchText) ||
+            this.removeAccents(
+              (suplencia.concurrencia_anual || '').toLowerCase()
+            ).includes(lowerSearchText) ||
+            this.removeAccents(
+              (suplencia.nombreCuidador || '').toLowerCase()
+            ).includes(lowerSearchText) ||
+            this.removeAccents(
+              (suplencia.apPatCuidador || '').toLowerCase()
+            ).includes(lowerSearchText) ||
+            this.removeAccents(
+              (suplencia.apMatCuidador || '').toLowerCase()
+            ).includes(lowerSearchText)
+        );
+      }
+  
+      return of(filteredData);
+    }
+  
+    applyFilteredData(filteredData: any): void {
+      if (this.selectedCategory === 'pacientes') {
+        this.filteredPacientes = filteredData;
+      } else if (this.selectedCategory === 'cuidadores') {
+        this.filteredCuidadores = filteredData;
+      } else if (this.selectedCategory === 'suplencias') {
+        this.filteredSuplencias = filteredData;
+      }
+    }
 }
